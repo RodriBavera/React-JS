8f66905c8e24362e57dad409f91e0b42cfe290ce
Rodrigo <rodri.crzz@gmail.com>
2024-12-28 16:48:36 -0300

    Ptimera PreEntrega Mejorada

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./componentes/NavBar/NavBar"
 import TituloPrincipal from "./componentes/TituloPrincipal/TituloPrincipal"
+import ItemListContainer from "./componentes/ItemListContainer/ItemListContainer"
 
 const App = () => {
   return (
     <div>
       <NavBar/>
       <TituloPrincipal texto= "BIENVENIDOS A LA TIENDA"/>
+      <ItemListContainer greeting="GRACIAS POR VISITAR NUESTA TIENDA"/>
     </div>
     
   )
